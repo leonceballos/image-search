f8556304aa97326cbd657e542234148a4e73489f
Leon Ceballos <leon_ceballos@epam.com>
2022-08-02 23:31:01 -0500

    test: added more SearchBar tests

--- src/components/SearchBar/__test__/SearchBar.test.tsx
+++ src/components/SearchBar/__test__/SearchBar.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
-
 import SearchBar from '../index'
 
 describe('SearchBar', () => {
@@ -12,5 +11,25 @@ describe('SearchBar', () => {
     const placeholder = 'Type any word.'
     render(<SearchBar onSearch={() => {}} placeholder={placeholder} />)
     expect(screen.getByPlaceholderText(placeholder)).toBeInTheDocument()
+  })
+  test('adds color clases on focus', () => {
+    render(<SearchBar onSearch={() => {}} />)
+    const inputText = screen.getByRole('textbox')
+    fireEvent.focus(inputText)
+
+    expect(inputText).toHaveClass('focus:border-b-sky-300')
+  })
+  test('updates the input value and call the onSearch callback handler', () => {
+    const userInput = 'cats'
+    const onSearch = jest.fn()
+    render(<SearchBar onSearch={onSearch} />)
+
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: userInput }
+    })
+    expect(screen.getByRole('textbox')).toHaveValue(userInput)
+
+    fireEvent.submit(screen.getByRole('textbox'))
+    expect(onSearch).toHaveBeenCalledTimes(1)
   })
 })
